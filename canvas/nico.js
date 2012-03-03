85667ca3df76317455e8e17ff0e964cd5a0e4912
菊池佑太 <Yuta.Kikuchi@gmail.com>
2012-03-03 23:00:12 +0900

    commit

--- canvas/nico.js
+++ canvas/nico.js
@@ -1,28 +1,42 @@
 (function(){
+    Nico.init = function() {
+        if( Nico.attr.timer ) {
+            clearInterval( Nico.attr.timer );
+        }
+        Nico.attr.timer = setInterval( 'Nico.play()', 25 );
+    };
+
     Nico.getObject = function() {
         return document.getElementById( 'canvas' ).getContext( '2d' );
     };
 
     Nico.play = function() {
-        Nico.attr.Object.fillText( Nico.attr.text, Nico.attr.start.x - Nico.attr.offset ,50 );
-        Nico.attr.offset += 20;
-        Nico.play();
+        Nico.remove();
+        for( var i=0; i<=Nico.attr.text.length-1; i++ ) {
+            var data = Nico.attr.text[i];
+            Nico.attr.Object.fillStyle = 'black';
+            Nico.attr.Object.fillText( data.node, data.start.x - data.offset , data.start.y );
+            Nico.attr.text[i].offset += 7;
+        }
     };
 
     Nico.remove = function() {
-        obj = Nico.attr.Object;
-        obj.fillStyle = 'white';
-        obj.clearRect( 0, 0, 800, 800 );
-        Nico.attr.offset += 20;
-        //Nico.play();
-    }
+        Nico.attr.Object.fillStyle = 'white';
+        Nico.attr.Object.clearRect( 0, 0, 800, 800 );
+    };
 
     Nico.attr = {};
     Nico.attr.Object = Nico.getObject();
     Nico.attr.Object.font = '40pt Arial';
-    Nico.attr.text = {};
-    Nico.attr.text = 'ああああああ';
-    Nico.attr.start = { x:500, y:80 };
-    Nico.attr.offset = 0;
-
+    Nico.attr.text = Array();
+    for( var i = 0; i<=10; i++ ) {
+        Nico.attr.text[i] = {};
+        var prefix = '';
+        for( var j = 0; j<=i; j++ ) {
+            prefix += '    ';
+        }
+        Nico.attr.text[i].node = prefix + "(´；ω；`)";
+        Nico.attr.text[i].start = { x:800, y:80*i };
+        Nico.attr.text[i].offset = 0;
+    }
 })();
